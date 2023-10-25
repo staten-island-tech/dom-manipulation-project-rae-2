1d83e721833eb52bc98ae6f11da72786748702c8
rae-2 <cloudandelion@gmail.com>
2023-10-25 12:46:04 -0400

    i think this is ok but how do i ADDDDDDDDDDDDDDDDDD

--- dom.js
+++ dom.js
@@ -1,18 +1,13 @@
-/* 
-const button = document.getElementById("btn");
-const text = document.querySelector("#text");
-console.log(text); */
-
-const DOMSelectors = {
-    button: document.getElementById("btn"),
-    text: document.querySelector("#text"),
-    box: document.getElementById("this is your album!!!!"),
+DOMSelectors = {
+    //store all realted code for selectinv elements here
+    form: document.querySelector("#form"),
+    cheese: document.querySelector(".cheese"),
+    h2s: document.querySelectorAll("h2"),
 };
 
-function backgrounds(background, text){
-    background.style.backgroundColor = "green";
-    text.innerHTML = "Tyarrrr"
-    text.style.fontSize = "40px";
-}
-
-backgrounds(DOMSelectors.box);
+DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    DOMSelectors.h2s.forEach(
+        (header)=> (header.textContent = DOMSelectors.cheese.value)
+    );    
+});
