7a7d73c2e0194e5924507db07b54f5774a490d9b
rae-2 <cloudandelion@gmail.com>
2023-10-31 13:03:04 -0400

    need to change soem stuff

--- dom.js
+++ dom.js
@@ -1,11 +1,11 @@
 const DOMSelectors = {
     //store all realted code for selectinv elements here
-    form: document.querySelector(".form"),
-    recname: document.getElementById("recname"),
+    form: document.querySelector("#form"),
+    nameofrecipe: document.getElementById("nameofrecipe"),
     picture: document.getElementById("picture"),
     recipe: document.getElementById("recipe"),
     button: document.getElementById("button"),
-    container: document.getElementById("container")
+    container: document.querySelector(".container")
 };
 
 DOMSelectors.form.addEventListener("submit", function (event) {
@@ -13,28 +13,10 @@ DOMSelectors.form.addEventListener("submit", function (event) {
     newstuff()
 });
 
-function newstuff() {
-    const whatever = {
-        name: DOMSelectors.recname.value,
-        picture: DOMSelectors.picture.value,
-        recipe: DOMSelectors.recipe.value,
-    };
-    insert(whatever, DOMSelectors);
-}
-
-function insert(whatever, DOMSelectors) {
-    DOMSelectors.container.insertAdjacentHTML("afterbegin",
-    `<div class="box">
-        <h2 class="the-card-title">${whatever.name}</h2>
-            <img src="${whatever.pictures}" alt="picture" class="image"></img>
-        <h2 class="therecipe">${whatever.recipe}</h2>
-    </div>`);
-    clear(DOMSelectors);
-}
-
 function clear(DOMSelectors){
-    DOMSelectors.recname.value = DOMSelectors.picture.value = DOMSelectors.recipe.value = "";
+    DOMSelectors.nameofrecipe.value = DOMSelectors.picture.value = DOMSelectors.recipe.value = "";
 };
+
 function remove(){
     let buttons = document.querySelectorAll("button");
     buttons.forEach((btn)=>
@@ -44,4 +26,23 @@ function remove(){
     );
 }
 
-remove(); 
+function newstuff() {
+    const whatever = {
+        name: DOMSelectors.nameofrecipe.value,
+        picture: DOMSelectors.picture.value,
+        recipe: DOMSelectors.recipe.value,
+    };
+    insert(whatever, DOMSelectors);
+}
+
+function insert(whatever, DOMSelectors) {
+    DOMSelectors.container.insertAdjacentHTML("afterbegin",
+    `<div class="parentcard">
+        <h2 class="card-title">${whatever.name}</h2>
+            <img src="${whatever.picture}" alt="picture" class="image"></img>
+        <h2 class="card-price">${whatever.recipe}</h2>
+    </div>`);
+    clear(DOMSelectors);
+    remove();
+}
+
